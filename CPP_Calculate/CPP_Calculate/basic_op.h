4fd3986aee2bba86bacb3ca22aeb89a74c4aa9ad
beonseong7 <beonseong7@naver.com>
2023-02-27 16:12:45 +0900

    2023.02.27 header file calculate

---
+++ CPP_Calculate/CPP_Calculate/basic_op.h
@@ -0,0 +1,7 @@
+#ifndef basic_OP
+#define basic_OP
+int plus_(int num1, int num2);
+int minus_(int num1, int num2);
+int multiple_(int num1, int num2);
+int divide_(int num1, int num2);
+#endif
---
+++ CPP_Calculate/CPP_Calculate/operator3.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include"basic_op.h"
+int main()
+{
+	using namespace std;
+	char op;
+	int num1 = 0;
+	int num2 = 0;
+	cout << "input +,-,/.*" << endl;
+	cin >> op;
+	cout << "input num1,num2" << endl;
+	cin >> num1 >> num2;
+
+	switch (op)
+	{
+	case '+':
+		cout << plus_(num1, num2) << endl;
+		break;
+	case '-':
+		cout << minus_(num1, num2) << endl;
+		break;
+	case '*':
+		cout << multiple_(num1, num2) << endl;
+		break;
+	case '/':
+		cout << divide_(num1, num2) << endl;
+		break;
+	default:
+		cout << "please enter correct op" << endl;
+		break;
+	}
+	return 0;
+}
