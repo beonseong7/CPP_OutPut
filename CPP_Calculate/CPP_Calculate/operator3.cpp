bafae2902390d76abb640f90b4bc51d93357cf4d
beonseong7 <beonseong7@naver.com>
2023-02-28 21:20:51 +0900

    2023.02.28 multiple input calculate

--- CPP_Calculate/CPP_Calculate/operator3.cpp
+++ CPP_Calculate/CPP_Calculate/operator3.cpp
@@ -7,9 +7,7 @@ int main()
 	int num1 = 0;
 	int num2 = 0;
 	cout << "input +,-,/.*" << endl;
-	cin >> op;
-	cout << "input num1,num2" << endl;
-	cin >> num1 >> num2;
+	cin >> num1 >> op >> num2;
 
 	switch (op)
 	{
