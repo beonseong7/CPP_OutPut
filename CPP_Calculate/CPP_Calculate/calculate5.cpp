0d960b7a306f4844307c4ef3edc37e78a2b36968
beonseong7 <beonseong7@naver.com>
2023-03-03 11:48:49 +0900

    2023.03.03 single digit calculater

--- CPP_Calculate/CPP_Calculate/calculate5.cpp
+++ CPP_Calculate/CPP_Calculate/calculate5.cpp
@@ -1,22 +1,24 @@
 #include<iostream>
 #include<stack>
 #include<string>
+#include"basic_op.h"
 using namespace std;
 int main()
 {
 	string input;
 	stack<char>op; 
-
+	stack<int> pf_op;
+	string postfix = "";
+	int tmp1, tmp2;
 	cout << "input formula" << endl;
 	cin >> input;
-	cout << input << input.size() << endl;
 	for (int i = 0; i < input.size(); i++) {
 		switch (input[i])
 		{
 		case '+':
 			for (int j = op.size(); j > 0 && op.top()!='('; j--)
 			{
-				cout << op.top();
+				postfix+=op.top();
 				op.pop();
 			}
 			op.push(input[i]);
@@ -24,7 +26,7 @@ int main()
 		case '-':
 			for (int j = op.size(); j > 0 && op.top() != '('; j--)
 			{
-				cout << op.top();
+				postfix+=op.top();
 				op.pop();
 			}
 			op.push(input[i]);
@@ -41,18 +43,58 @@ int main()
 		case')':
 			for (int j = op.size(); op.top()!='('; j--)
 			{
-				cout << op.top();
+				postfix+= op.top();
 				op.pop();
 			}
 			op.pop();
 			break;
 		default:
-			cout << input[i];
+			postfix+= input[i];
 			break;
 		}
 	}
 	while (op.size() > 0) {
-		cout << op.top();
+		postfix+= op.top();
 		op.pop();
 	}
+	cout << postfix << endl;
+	for (int i = 0; i < postfix.size(); i++)
+	{
+		switch (postfix[i]) {
+		case'+':
+			
+			tmp1= pf_op.top();
+			pf_op.pop();
+			tmp2 = pf_op.top();
+			pf_op.pop();
+			pf_op.push(plus_(tmp1, tmp2));
+			break;
+		case'-':
+			tmp1 = pf_op.top();
+			pf_op.pop();
+			tmp2 = pf_op.top();
+			pf_op.pop();
+			pf_op.push(minus_(tmp1, tmp2));
+			break;
+		case'/':
+			tmp2 = pf_op.top();
+			pf_op.pop();
+			tmp1 = pf_op.top();
+			pf_op.pop();
+			pf_op.push(divide_(tmp1, tmp2));
+			break;
+		case'*':
+			tmp1 = pf_op.top();
+			pf_op.pop();
+			tmp2 = pf_op.top();
+			pf_op.pop();
+			pf_op.push(multiple_(tmp1, tmp2));
+			break;
+		default:
+			pf_op.push(postfix[i]-'0');
+			break;
+		}
+		cout << "done" << endl;
+	}
+	cout << "answer is" << pf_op.top() << endl;
 }
