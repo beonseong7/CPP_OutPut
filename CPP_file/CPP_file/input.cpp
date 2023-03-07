0cb970ba1cbdf9b30766e14157b626659e3c20ba
beonseong7 <beonseong7@naver.com>
2023-03-07 17:53:36 +0900

    2023.03.07 txt save function

---
+++ CPP_file/CPP_file/input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+int main()
+{
+	using namespace std;
+	string data;
+	cout << "input stirng" << endl;
+	cin >> data;
+	cout << "your save str is" << data << endl;
+
+	return 0;
+}
