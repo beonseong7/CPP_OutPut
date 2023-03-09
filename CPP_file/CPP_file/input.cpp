ea72051a8bf48c52a805ca1ad15eb9509ac266e5
beonseong7 <beonseong7@naver.com>
2023-03-09 19:45:19 +0900

    2023.03.09 student struct

--- CPP_file/CPP_file/input.cpp
+++ CPP_file/CPP_file/input.cpp
@@ -1,12 +1,19 @@
 #include<iostream>
 #include<string>
+#include "student.h"
 int main()
 {
 	using namespace std;
 	string data;
-	cout << "input stirng" << endl;
-	cin >> data;
-	cout << "your save str is" << data << endl;
-
+	student students[10];
+	for (int i = 0; i < 3; i++) {
+		string tmp1, tmp2, tmp3;
+		cout << "name school id(ex: kimpark 201512165 )" << endl;
+		cin >> students[i].name >> students[i].id;
+	}
+	for (int i = 0; i < 3; i++) {
+		cout << students[i].id << endl;
+		cout << students[i].name << endl;
+	}
 	return 0;
 }
