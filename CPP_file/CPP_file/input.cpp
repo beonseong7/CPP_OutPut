185b823c5e01d5725c79c3c94b46e05d65e857b5
beonseong7 <beonseong7@naver.com>
2023-03-10 17:53:30 +0900

    2023.03.10 struct function

--- CPP_file/CPP_file/input.cpp
+++ CPP_file/CPP_file/input.cpp
@@ -1,19 +1,37 @@
 #include<iostream>
 #include<string>
 #include "student.h"
+#define  input 3
+student init_student();
+bool search_student(student tmp1,student tmp2);
 int main()
 {
 	using namespace std;
 	string data;
 	student students[10];
-	for (int i = 0; i < 3; i++) {
-		string tmp1, tmp2, tmp3;
-		cout << "name school id(ex: kimpark 201512165 )" << endl;
-		cin >> students[i].name >> students[i].id;
+	for (int i = 0; i < input; i++) {
+		students[i] = init_student();
 	}
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < input; i++) {
 		cout << students[i].id << endl;
 		cout << students[i].name << endl;
 	}
 	return 0;
+}
+student init_student()
+{
+	student tmp;
+	cout << "name school id(ex: kimpark 201512165 )" << endl;
+	cin >> tmp.name >> tmp.id;
+	return tmp;
+}
+bool search_student(student tmp1,student tmp2)
+{
+	if (tmp1.id == tmp2.id && tmp2.id == tmp2.name) {
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
